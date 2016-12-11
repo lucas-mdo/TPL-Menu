85a54cddb79867b6f58ef3227f5371907da897f9
lucas-mdo <lucas.mdo92@gmail.com>
2016-12-11 19:21:28 -0200

    New struct types for mainMenu and subMenu.

--- LcdMenu/LcdMenu.ino
+++ LcdMenu/LcdMenu.ino
@@ -1,20 +1,9 @@
-// You can have up to 10 menu items in the menuItems[] array below without having to change the base programming at all. Name them however you'd like. Beyond 10 items, you will have to add additional "cases" in the switch/case
+// You can have up to 10 menu items in the mainMenuItems[] array below without having to change the base programming at all. Name them however you'd like. Beyond 10 items, you will have to add additional "cases" in the switch/case
 // section of the operateMainMenu() function below. You will also have to add additional void functions (i.e. menuItem11, menuItem12, etc.) to the program.
-String menuItems[] = { "Voltage", "Pulses", "Duration", "Delay", "Confirm" };
+String mainMenuItems[] = { "Voltage", "Pulses", "Duration", "Delay", "Confirm" };
 
 // Navigation button variables
 int readKey;
-
-// Menu control variables
-int menuPage = 0;
-int maxMenuPages = round(((sizeof(menuItems) / sizeof(String)) / 2) + .5);
-int cursorPosition = 0;
-
-// SubMenu 1 control variables
-int subMenu1Page = 0;
-int maxSubMenu1Pages = round(((sizeof(subMenuVoltage) / sizeof(String)) / 2) + .5);
-int subMenu1cursorPosition = 0;
-int savedVoltage = 0;
 
 // DEBUG LEDS ----------------------------
 int led0pin = 43;
@@ -25,20 +14,31 @@ int led4pin = 51;
 int led5pin = 53;
 
 typedef struct {
-	String title;
-	String unit;
+	char title[11]; //10 char + 1 end-string
+	char unit[4]; //3 char + 1 end-string
 	int storedValue;
 	int minValue;
 	int maxValue;
-	int increment;
-} mySubMenu;
+	int incValue;
+} subMenu;
 
-void subMenu(mySubMenu *s);
+// Menu control variables
+typedef struct {
+	int menuPage;
+	int maxMenuPages;
+	int cursorPosition;
+} displayMenu;
 
-mySubMenu voltageMenu{ "Voltage: ",   " V",   0, 0, 50, 10 };
-mySubMenu pulseMenu{ "Pulses: ",    " s",   0, 0, 5,  1 };
-mySubMenu durationMenu{ "Duration: ",  " ms",  0, 0, 10, 2 };
-mySubMenu delayMenu{ "Delay: ",     " ms",  0, 0, 10, 2 };
+int getMaxMenuPages(displayMenu &d) { return round(((sizeof(mainMenuItems) / sizeof(String)) / 2) + .5); }
+
+int getTitleLength(subMenu &s) { return strlen(s.title); }
+
+displayMenu mainMenu = { 0, 0, 0 };
+
+subMenu voltageMenu = { "Voltage: ", " V", 0, 0, 50, 10 };
+subMenu pulseMenu = { "Pulses: ", " s", 0, 0, 5, 1 };
+subMenu durationMenu = { "Duration: ", " ms", 0, 0, 10, 2 };
+subMenu delayMenu = { "Delay: ", " ms", 0, 0, 10, 2 };
 
 // Creates 3 custom characters for the menu display
 byte downArrow[8] = {
@@ -93,7 +93,7 @@ void setup() {
 	pinMode(led4pin, OUTPUT);
 	pinMode(led5pin, OUTPUT);
 
-	// Initializes and clears the LCD screen
+	// Initializes && clears the LCD screen
 	lcd.begin(16, 2);
 	lcd.clear();
 
@@ -101,15 +101,17 @@ void setup() {
 	lcd.createChar(0, menuCursor);
 	lcd.createChar(1, upArrow);
 	lcd.createChar(2, downArrow);
+
+	mainMenu.maxMenuPages = getMaxMenuPages(mainMenu);
 }
 
 void loop() {
-	menuDraw(menuPage, maxMenuPages, menuItems);
-	drawCursor(menuPage, cursorPosition);
+	menuDraw(mainMenu.menuPage, mainMenu.maxMenuPages, mainMenuItems);
+	drawCursor(mainMenu.menuPage, mainMenu.cursorPosition);
 	operateMainMenu();
 }
 
-// This function will generate the 2 menu items that can fit on the screen. They will change as you scroll through your menu. Up and down arrows will indicate your current menu position.
+// This function will generate the 2 menu items that can fit on the screen. They will change as you scroll through your menu. Up && down arrows will indicate your current menu position.
 void menuDraw(int page, int maxPages, String itemsArray[]) {
 	Serial.print(page);
 	lcd.clear();
@@ -121,7 +123,7 @@ void menuDraw(int page, int maxPages, String itemsArray[]) {
 		lcd.setCursor(15, 1);
 		lcd.write(byte(2));
 	}
-	else if (page > 0 and page < maxPages) {
+	else if (page > 0 && page < maxPages) {
 		lcd.setCursor(15, 1);
 		lcd.write(byte(2));
 		lcd.setCursor(15, 0);
@@ -133,53 +135,7 @@ void menuDraw(int page, int maxPages, String itemsArray[]) {
 	}
 }
 
-void mainMenuDraw() {
-	Serial.print(menuPage);
-	lcd.clear();
-	lcd.setCursor(1, 0);
-	lcd.print(menuItems[menuPage]);
-	lcd.setCursor(1, 1);
-	lcd.print(menuItems[menuPage + 1]);
-	if (menuPage == 0) {
-		lcd.setCursor(15, 1);
-		lcd.write(byte(2));
-	}
-	else if (menuPage > 0 and menuPage < maxMenuPages) {
-		lcd.setCursor(15, 1);
-		lcd.write(byte(2));
-		lcd.setCursor(15, 0);
-		lcd.write(byte(1));
-	}
-	else if (menuPage == maxMenuPages) {
-		lcd.setCursor(15, 0);
-		lcd.write(byte(1));
-	}
-}
-
-void subMenu1Draw() {
-	Serial.print(subMenu1Page);
-	lcd.clear();
-	lcd.setCursor(1, 0);
-	lcd.print(subMenuVoltage[subMenu1Page]);
-	lcd.setCursor(1, 1);
-	lcd.print(subMenuVoltage[subMenu1Page + 1]);
-	if (subMenu1Page == 0) {
-		lcd.setCursor(15, 1);
-		lcd.write(byte(2));
-	}
-	else if (subMenu1Page > 0 and subMenu1Page < maxSubMenu1Pages) {
-		lcd.setCursor(15, 1);
-		lcd.write(byte(2));
-		lcd.setCursor(15, 0);
-		lcd.write(byte(1));
-	}
-	else if (subMenu1Page == maxSubMenu1Pages) {
-		lcd.setCursor(15, 0);
-		lcd.write(byte(1));
-	}
-}
-
-// When called, this function will erase the current cursor and redraw it based on the cursorPosition and menuPage variables.
+// When called, this function will erase the current cursor && redraw it based on the cursorPosition && menuPage variables.
 void drawCursor(int page, int pos) {
 	for (int x = 0; x < 2; x++) {     // Erases current cursor
 		lcd.setCursor(0, x);
@@ -187,83 +143,23 @@ void drawCursor(int page, int pos) {
 	}
 
 	// The menu is set up to be progressive (menuPage 0 = Item 1 & Item 2, menuPage 1 = Item 2 & Item 3, menuPage 2 = Item 3 & Item 4), so
-	// in order to determine where the cursor should be you need to see if you are at an odd or even menu page and an odd or even cursor position.
+	// in order to determine where the cursor should be you need to see if you are at an odd or even menu page && an odd or even cursor position.
 	if (page % 2 == 0) {
-		if (pos % 2 == 0) {  // If the menu page is even and the cursor position is even that means the cursor should be on line 1
+		if (pos % 2 == 0) {  // If the menu page is even && the cursor position is even that means the cursor should be on line 1
 			lcd.setCursor(0, 0);
 			lcd.write(byte(0));
 		}
-		if (pos % 2 != 0) {  // If the menu page is even and the cursor position is odd that means the cursor should be on line 2
+		if (pos % 2 != 0) {  // If the menu page is even && the cursor position is odd that means the cursor should be on line 2
 			lcd.setCursor(0, 1);
 			lcd.write(byte(0));
 		}
 	}
 	if (page % 2 != 0) {
-		if (pos % 2 == 0) {  // If the menu page is odd and the cursor position is even that means the cursor should be on line 2
+		if (pos % 2 == 0) {  // If the menu page is odd && the cursor position is even that means the cursor should be on line 2
 			lcd.setCursor(0, 1);
 			lcd.write(byte(0));
 		}
-		if (pos % 2 != 0) {  // If the menu page is odd and the cursor position is odd that means the cursor should be on line 1
-			lcd.setCursor(0, 0);
-			lcd.write(byte(0));
-		}
-	}
-}
-
-void drawMenuCursor() {
-	for (int x = 0; x < 2; x++) {     // Erases current cursor
-		lcd.setCursor(0, x);
-		lcd.print(" ");
-	}
-
-	// The menu is set up to be progressive (menuPage 0 = Item 1 & Item 2, menuPage 1 = Item 2 & Item 3, menuPage 2 = Item 3 & Item 4), so
-	// in order to determine where the cursor should be you need to see if you are at an odd or even menu page and an odd or even cursor position.
-	if (menuPage % 2 == 0) {
-		if (cursorPosition % 2 == 0) {  // If the menu page is even and the cursor position is even that means the cursor should be on line 1
-			lcd.setCursor(0, 0);
-			lcd.write(byte(0));
-		}
-		if (cursorPosition % 2 != 0) {  // If the menu page is even and the cursor position is odd that means the cursor should be on line 2
-			lcd.setCursor(0, 1);
-			lcd.write(byte(0));
-		}
-	}
-	if (menuPage % 2 != 0) {
-		if (cursorPosition % 2 == 0) {  // If the menu page is odd and the cursor position is even that means the cursor should be on line 2
-			lcd.setCursor(0, 1);
-			lcd.write(byte(0));
-		}
-		if (cursorPosition % 2 != 0) {  // If the menu page is odd and the cursor position is odd that means the cursor should be on line 1
-			lcd.setCursor(0, 0);
-			lcd.write(byte(0));
-		}
-	}
-}
-
-void drawSubMenu1Cursor() {
-	for (int x = 0; x < 2; x++) {     // Erases current cursor
-		lcd.setCursor(0, x);
-		lcd.print(" ");
-	}
-
-	// The menu is set up to be progressive (menuPage 0 = Item 1 & Item 2, menuPage 1 = Item 2 & Item 3, menuPage 2 = Item 3 & Item 4), so
-	// in order to determine where the cursor should be you need to see if you are at an odd or even menu page and an odd or even cursor position.
-	if (subMenu1Page % 2 == 0) {
-		if (subMenu1cursorPosition % 2 == 0) {  // If the menu page is even and the cursor position is even that means the cursor should be on line 1
-			lcd.setCursor(0, 0);
-			lcd.write(byte(0));
-		}
-		if (subMenu1cursorPosition % 2 != 0) {  // If the menu page is even and the cursor position is odd that means the cursor should be on line 2
-			lcd.setCursor(0, 1);
-			lcd.write(byte(0));
-		}
-	}
-	if (subMenu1Page % 2 != 0) {
-		if (subMenu1cursorPosition % 2 == 0) {  // If the menu page is odd and the cursor position is even that means the cursor should be on line 2
-			lcd.setCursor(0, 1);
-			lcd.write(byte(0));
-		}
-		if (subMenu1cursorPosition % 2 != 0) {  // If the menu page is odd and the cursor position is odd that means the cursor should be on line 1
+		if (pos % 2 != 0) {  // If the menu page is odd && the cursor position is odd that means the cursor should be on line 1
 			lcd.setCursor(0, 0);
 			lcd.write(byte(0));
 		}
@@ -285,21 +181,21 @@ void operateMainMenu() {
 			break;
 		case 1:  // This case will execute if the "forward" button is pressed
 			button = 0;
-			switch (cursorPosition) { // The case that is selected here is dependent on which menu page you are on and where the cursor is.
+			switch (mainMenu.cursorPosition) { // The case that is selected here is dependent on which menu page you are on && where the cursor is.
 			case 0:
-				menuItem1(); //Voltage
+				showSubMenu(voltageMenu);
 				break;
 			case 1:
-				menuItem2(); //Pulses
+				menuItem2();
 				break;
 			case 2:
-				menuItem3(); //Duration
+				menuItem3();
 				break;
 			case 3:
-				menuItem4(); //Delay
+				menuItem4();
 				break;
 			case 4:
-				menuItem5(); //Confirm
+				menuItem5();
 				break;
 			case 5:
 				menuItem6();
@@ -318,17 +214,17 @@ void operateMainMenu() {
 				break;
 			}
 			activeButton = 1;
-			mainMenuDraw();
-			drawCursor(menuPage, cursorPosition);
+			menuDraw(mainMenu.menuPage, mainMenu.maxMenuPages, mainMenuItems);
+			drawCursor(mainMenu.menuPage, mainMenu.cursorPosition);
 			break;
 		case 2:
 			button = 0;
-			moveUp(menuPage, maxMenuPages, cursorPosition, menuItems);
+			moveUp(mainMenu, mainMenuItems);
 			activeButton = 1;
 			break;
 		case 3:
 			button = 0;
-			moveDown(menuPage, maxMenuPages, cursorPosition, menuItems);
+			moveDown(mainMenu, mainMenuItems);
 			activeButton = 1;
 			break;
 		}
@@ -365,100 +261,56 @@ void drawInstructions() {
 	lcd.print(" buttons");
 }
 
-//The following couple of functions handles the navigations through menu items vertically (up and down)
-void moveUp(int &page, int &maxPages, int &pos, String itemsArray[]) {
-	if (page == 0) {
-		pos = pos - 1;
-		pos = constrain(pos, 0, ((sizeof(itemsArray) / sizeof(String)) - 1));
+//The following couple of functions handles the navigations through menu items vertically (up && down)
+void moveUp(displayMenu &d, String itemsArray[]) {
+	if (d.menuPage == 0) {
+		d.cursorPosition = d.cursorPosition - 1;
+		d.cursorPosition = constrain(d.cursorPosition, 0, ((sizeof(itemsArray) / sizeof(String)) - 1));
 	}
-	if (page % 2 == 0 and pos % 2 == 0) {
-		page = page - 1;
-		page = constrain(page, 0, maxPages);
-	}
-
-	if (page % 2 != 0 and pos % 2 != 0) {
-		page = page - 1;
-		page = constrain(page, 0, maxPages);
+	if (d.menuPage % 2 == 0 && d.cursorPosition % 2 == 0) {
+		d.menuPage = d.menuPage - 1;
+		d.menuPage = constrain(d.menuPage, 0, d.maxMenuPages);
 	}
 
-	pos = pos - 1;
-	pos = constrain(pos, 0, ((sizeof(itemsArray) / sizeof(String)) - 1));
+	if (d.menuPage % 2 != 0 && d.cursorPosition % 2 != 0) {
+		d.menuPage = d.menuPage - 1;
+		d.menuPage = constrain(d.menuPage, 0, d.maxMenuPages);
+	}
 
-	menuDraw(page, maxPages, itemsArray);
-	drawCursor(page, pos);
+	d.cursorPosition = d.cursorPosition - 1;
+	d.cursorPosition = constrain(d.cursorPosition, 0, ((sizeof(itemsArray) / sizeof(String)) - 1));
+
+	menuDraw(d.menuPage, d.maxMenuPages, itemsArray);
+	drawCursor(d.menuPage, d.cursorPosition);
 }
 
-void moveDown(int &page, int &maxPages, int &pos, String itemsArray[]) {
-	if (page % 2 == 0 and pos % 2 != 0) {
-		page = page + 1;
-		page = constrain(page, 0, maxPages);
+void moveDown(displayMenu &d, String itemsArray[]) {
+	if (d.menuPage % 2 == 0 && d.cursorPosition % 2 != 0) {
+		d.menuPage = d.menuPage + 1;
+		d.menuPage = constrain(d.menuPage, 0, d.maxMenuPages);
 	}
 
-	if (page % 2 != 0 and pos % 2 == 0) {
-		page = page + 1;
-		page = constrain(page, 0, maxPages);
+	if (d.menuPage % 2 != 0 && d.cursorPosition % 2 == 0) {
+		d.menuPage = d.menuPage + 1;
+		d.menuPage = constrain(d.menuPage, 0, d.maxMenuPages);
 	}
 
-	pos = pos + 1;
-	pos = constrain(pos, 0, ((sizeof(itemsArray) / sizeof(String)) - 1));
+	d.cursorPosition = d.cursorPosition + 1;
+	d.cursorPosition = constrain(d.cursorPosition, 0, ((sizeof(itemsArray) / sizeof(String)) - 1));
 
-	menuDraw(page, maxPages, itemsArray);
-	drawCursor(page, pos);
+	menuDraw(d.menuPage, d.maxMenuPages, itemsArray);
+	drawCursor(d.menuPage, d.cursorPosition);
 }
 
-void menuItem1Options() { // Function executes when you select the 1st item from main menu
-	int activeButton = 0;
-
-	lcd.clear();
-	lcd.setCursor(3, 0);
-	//lcd.print("Choose Value");
-	menuDraw(subMenu1Page, maxSubMenu1Pages, subMenuVoltage);
-	drawCursor(subMenu1Page, subMenu1cursorPosition);
-
-	while (activeButton == 0) {
-		int button;
-		readKey = analogRead(0);
-		if (readKey < 790) {
-			delay(100);
-			readKey = analogRead(0);
-		}
-		button = evaluateButton(readKey);
-		switch (button) {
-		case 1:
-			chosenVoltage = subMenu1cursorPosition;
-			button = 0;
-			activeButton = 1;
-			break;
-
-		case 2:
-			button = 0;
-			moveUp(subMenu1Page, maxSubMenu1Pages, subMenu1cursorPosition, subMenuVoltage);
-			//activeButton = 1;
-			break;
-
-		case 3:
-			button = 0;
-			moveDown(subMenu1Page, maxSubMenu1Pages, subMenu1cursorPosition, subMenuVoltage);
-			//activeButton = 1;
-			break;
-
-		case 4:  // This case will execute if the "back" button is pressed
-			button = 0;
-			activeButton = 1;
-			break;
-		}
-	}
-}
-
-void menuItem1(String title, String unit) { // Function executes when you select the 1st item from main menu
+void showSubMenu(subMenu &s) { // Function executes when you select the 1st item from main menu
 	int activeButton = 0;
 	lcd.clear();
 	lcd.setCursor(0, 1);
 	drawInstructions();
 	lcd.setCursor(0, 0);
-	lcd.print(title);
-	lcd.print(savedVoltage);
-	lcd.print(unit);
+	lcd.print(s.title);
+	lcd.print(s.storedValue);
+	lcd.print(s.unit);
 	while (activeButton == 0) {
 		int button;
 		readKey = analogRead(0);
@@ -470,23 +322,23 @@ void menuItem1(String title, String unit) { // Function executes when you select
 		switch (button) {
 		case 2:
 			button = 0;
-			savedVoltage = savedVoltage + 10;
-			savedVoltage = constrain(savedVoltage, 0, 50);
-			lcd.setCursor(title.length, 0);
+			s.storedValue = s.storedValue + s.incValue;
+			s.storedValue = constrain(s.storedValue, s.minValue, s.maxValue);
+			lcd.setCursor(getTitleLength(s), 0);
 			lcd.print("     ");
-			lcd.setCursor(title.length, 0);
-			lcd.print(savedVoltage);
-			lcd.print(unit);
+			lcd.setCursor(getTitleLength(s), 0);
+			lcd.print(s.storedValue);
+			lcd.print(s.unit);
 			break;
 		case 3:
 			button = 0;
-			savedVoltage = savedVoltage - 10;
-			savedVoltage = constrain(savedVoltage, 0, 50);
-			lcd.setCursor(9, 0);
+			s.storedValue = s.storedValue - s.incValue;
+			s.storedValue = constrain(s.storedValue, s.minValue, s.maxValue);
+			lcd.setCursor(getTitleLength(s), 0);
 			lcd.print("     ");
-			lcd.setCursor(9, 0);
-			lcd.print(savedVoltage);
-			lcd.print(unit);
+			lcd.setCursor(getTitleLength(s), 0);
+			lcd.print(s.storedValue);
+			lcd.print(s.unit);
 			break;
 		case 4:  // This case will execute if the "back" button is pressed
 			button = 0;
@@ -576,9 +428,9 @@ void menuItem4() { // Function executes when you select the 4th item from main m
 
 void menuItem5() { // Function executes when you select the 5th item from main menu
 	Serial.print("Chosen voltage: ");
-	Serial.println(chosenVoltage);
+	Serial.println(voltageMenu.storedValue);
 
-	switch (chosenVoltage) {
+	switch (voltageMenu.storedValue) {
 	case 0:
 		Serial.println("Turning on led 0");
 		digitalWrite(led0pin, HIGH);
